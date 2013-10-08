587a4e646480463169ec4f6284170c1784a6f6e1
softvar <varun2902@gmail.com>
2013-10-09 01:08:35 +0530

    optimized code

--- json2html/jsonconv.py
+++ json2html/jsonconv.py
@@ -12,7 +12,10 @@ class JSON:
 		'''
 		if 'json' in args:
 			self.json_input = args['json']
-			self.json_input = json.dumps(self.json_input)
+			try:
+				json.loads(self.json_input)
+			except:
+				self.json_input = json.dumps(self.json_input)
 		else:
 			raise Exception('Can\'t convert NULL!')
 		
@@ -48,7 +51,59 @@ class JSON:
 				print '</td></tr>'
 		print '</table>'
 
+	def iterJson(self,ordered_json):
+		global a
+		a=a+ "<table border=\"1\">" 
+		for k,v in ordered_json.iteritems():
+			a=a+ '<tr>'
+			a=a+ '<th>'+ str(k) +'</th>'
+			if(isinstance(v,list)):
+				a=a+ '<td><ul>'
+				for i in range(0,len(v)):
+					if(isinstance(v[i],unicode)):
+						a=a+ '<li>'+str(v[i])+'</li>'
+					elif(isinstance(v[i],int) or isinstance(v,float)):
+						a=a+ '<li>'+str(v[i])+'</li>'
+					elif(isinstance(v[i],list)==False):
+						self.iterJson(v[i])
+				a=a+ '</ul></td>'
+				a=a+ '</tr>'
+			elif(isinstance(v,unicode)):
+				a=a+ '<td>'+ str(v) +'</td>'
+				a=a+ '</tr>'
+			elif(isinstance(v,int) or isinstance(v,float)):
+				a=a+ '<td>'+ str(v) +'</td>'
+				a=a+ '</tr>'
+			else:
+				a=a+ '<td>'
+				#a=a+ '<table border="1">'
+				self.iterJson(v)
+				a=a+ '</td></tr>'
+		a=a+ '</table>'
+
 	def htmlConvertor(self,ordered_json):
+		'''
+		converts JSON Object into human readable HTML representation
+		generating HTML table code with raw/bootstrap styling.
+		'''
+		global a
+		try:
+			for k,v in ordered_json.iteritems():
+				pass
+			self.iterJson(ordered_json)
+		
+		except:
+			for i in range(0,len(ordered_json)):
+				if(isinstance(ordered_json[i],unicode)):
+					a=a+ '<li>'+str(ordered_json[i])+'</li>'
+				elif(isinstance(ordered_json[i],int) or isinstance(ordered_json[i],float)):
+					a=a+ '<li>'+str(ordered_json[i])+'</li>'
+				elif(isinstance(ordered_json[i],list)==False):
+					self.htmlConvertor(ordered_json[i])	
+
+		return a
+
+	def htmlConvertormain(self,ordered_json):
 		global a
 		a+='<table border="1">\n'
 		for k,v in ordered_json.iteritems():
