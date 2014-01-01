d13f577c0d6edf3363d8f17c058d0c2519059d88
softvar <varun2902@gmail.com>
2014-01-01 16:26:36 +0530

    version bump- 0.2.1 :sparkles:

--- setup.py
+++ setup.py
@@ -11,14 +11,14 @@ if sys.version_info[:2] < (2,6):
 setup(
     name = 'json2html',
     packages = ['json2html'],
-    version = '0.2',
+    version = '0.2.1',
     install_requires=required,
     description = 'JSON object to human readable HTML representation',
     long_description=open('README.rst').read(),
     author = 'Varun Malhotra',
     author_email = 'varun2902@gmail.com',
     url = 'https://github.com/softvar/json2html',
-    download_url = 'https://github.com/softvar/json2html/tarball/0.2',
+    download_url = 'https://github.com/softvar/json2html/tarball/0.2.1',
     keywords = ['json', 'HTML', 'Table'],
     license = 'MIT',
     classifiers = (
