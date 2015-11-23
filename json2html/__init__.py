cbfd76dbdca20fa2ec062588b66388d1b9121b16
Nikolai-Hlubek <nikolai.hlubek@gmail.com>
2015-11-23 11:38:46 +0100

    Branching for python 2 and 3
    Untested.

--- json2html/__init__.py
+++ json2html/__init__.py
@@ -3,7 +3,12 @@ python wrapper for JSON to HTML-Table convertor
 (c) 2013 Varun Malhotra. MIT License
 '''
 
-from jsonconv import *
+import sys
+
+if sys.version_info[0] == 2:
+  from jsonconv import *
+else:
+  from jsonconv3 import *
 
 __author__ = 'Varun Malhotra'
 __version__ = '0.3'
