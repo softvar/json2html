b86a5ee0a8716b63822cb4b5523d9897f380a6b1
r2h2 <rainer@hoerbe.at>
2016-05-12 08:54:46 +0200

    py3 compat

--- json2html/__init__.py
+++ json2html/__init__.py
@@ -3,7 +3,7 @@ python wrapper for JSON to HTML-Table convertor
 (c) 2013 Varun Malhotra. MIT License
 '''
 
-from jsonconv import *
+from .jsonconv import *
 
 __author__ = 'Varun Malhotra'
 __version__ = '1.0.1'
