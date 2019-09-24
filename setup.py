a6510074c2da10b1150059c1497631d9d39f7d44
hugovk <hugovk@users.noreply.github.com>
2019-09-24 21:23:23 +0300

    Add support for Python 3.6 and 3.7

--- setup.py
+++ setup.py
@@ -14,6 +14,13 @@ setup(
     keywords = ['json', 'HTML', 'Table'],
     license = 'MIT',
     python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, !=3.4.*',
-    classifiers = (
-    ),
+    classifiers = [
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: Implementation :: CPython',
+    ],
 )
